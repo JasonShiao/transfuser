3d609eedcd0e8752263a66b7d9222a3c43c3736b
Jau-Shiuan Shiao <b01901105@gmail.com>
2025-04-16 06:38:23 -0400

    Fixed GlobalRoutePlanner has no setup()

--- scenario_runner/srunner/scenariomanager/scenarioatomics/atomic_trigger_conditions.py
+++ scenario_runner/srunner/scenariomanager/scenarioatomics/atomic_trigger_conditions.py
@@ -108,7 +108,6 @@ class InTriggerDistanceToOSCPosition(AtomicCondition):
         if self._along_route:
             # Get the global route planner, used to calculate the route
             grp = GlobalRoutePlanner(self._map, 0.5)
-            grp.setup()
             self._grp = grp
         else:
             self._grp = None
@@ -174,9 +173,7 @@ class InTimeToArrivalToOSCPosition(AtomicCondition):
 
         if self._along_route:
             # Get the global route planner, used to calculate the route
-            dao = GlobalRoutePlannerDAO(self._map, 0.5)
-            grp = GlobalRoutePlanner(dao)
-            grp.setup()
+            grp = GlobalRoutePlanner(self._map, 0.5)
             self._grp = grp
         else:
             self._grp = None
@@ -844,9 +841,7 @@ class InTimeToArrivalToVehicle(AtomicCondition):
 
         if self._along_route:
             # Get the global route planner, used to calculate the route
-            dao = GlobalRoutePlannerDAO(self._map, 0.5)
-            grp = GlobalRoutePlanner(dao)
-            grp.setup()
+            grp = GlobalRoutePlanner(self._map, 0.5)
             self._grp = grp
         else:
             self._grp = None
